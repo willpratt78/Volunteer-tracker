3b19fe8e450c5f9108cde5284b647885f7fd647b
William Pratt <willpratt78@gmail.com>
2022-05-20 13:47:09 -0700

    good pass for .all

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -17,7 +17,15 @@ class Volunteer
   end
 
   def self.all
-   
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
   end
 
 end
