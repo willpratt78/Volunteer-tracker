dbcae2dd1bff72734fc712c21d09a266fc618614
William Pratt <willpratt78@gmail.com>
2022-05-20 09:41:46 -0700

    running into error   connection to server at localhost (::1), port 5432 failed: fe_sendauth: no password supplied trying to fix now

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,7 +4,7 @@ describe Project do
   describe '#title' do
     it 'returns the project title' do
       project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-      expect(project.title).to(eq('Teaching Kids to Code'))
+      expect(project.title).to eq 'Teaching Kids to Code'
     end
   end
 
