b33472da708980c39feef669b4713031b35ff803
William Pratt <willpratt78@gmail.com>
2022-05-20 10:24:25 -0700

    test #id was autopassing upon uncommenting it, will readdress if it fails at a later test

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,11 +8,12 @@ describe Project do
     end
   end
 
-  # context '#id' do
-  #   it 'returns the id of the project before saving project' do
-  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     expect(project.id).to eq nil
-  #   end
+  context '#id' do
+    it 'returns the id of the project before saving project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project.id).to eq nil
+    end
+  end
 
   #   it 'returns the id of the project after saving project' do
   #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
