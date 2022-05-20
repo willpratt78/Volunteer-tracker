7b683ba0242ed8cf2518f5e045a384625677cf83
William Pratt <willpratt78@gmail.com>
2022-05-20 10:15:44 -0700

    rename @name to @title

--- lib/project.rb
+++ lib/project.rb
@@ -3,11 +3,11 @@ class Project
   attr_accessor :title
 
   def initialize(attributes)
-    @name = attributes.fetch(:title)
+    @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
 
   def self.title
-    
+    title = DB.exec("SELECT * FROM projects").
   end
 end
