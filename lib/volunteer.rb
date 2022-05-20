cbbc5e4913717eb6668d64545bc5d5769ee58403
William Pratt <willpratt78@gmail.com>
2022-05-20 13:25:03 -0700

    good fail for name

---
+++ lib/volunteer.rb
@@ -0,0 +1,14 @@
+class Volunteer
+  attr_reader :id
+  attr_accessor :name, :project_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+  def name
+
+  end
+end
