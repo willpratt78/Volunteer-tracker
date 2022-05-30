31986f72e1a6e9fa7ef6aab0964dd6e2bb0f54c0
William Pratt <willpratt78@gmail.com>
2022-05-30 14:37:14 -0700

    change all instances of projects_id to project_id in volunteer.rb

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -21,7 +21,7 @@ class Volunteer
     volunteers = []
     returned_volunteers.each do |volunteer|
       name = volunteer.fetch("name")
-      project_id = volunteer.fetch("projects_id").to_i
+      project_id = volunteer.fetch("project_id").to_i
       id = volunteer.fetch("id").to_i
       volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
@@ -29,21 +29,21 @@ class Volunteer
   end
 
   def save
-    result = DB.exec("INSERT INTO volunteers (name, projects_id) VALUES ('#{name}', #{project_id}) RETURNING id;")
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{name}', #{project_id}) RETURNING id;")
     @id = result.first.fetch("id").to_i
   end
 
   def self.find(id)
     volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
     name = volunteer.fetch("name")
-    project_id = volunteer.fetch("projects_id").to_i
+    project_id = volunteer.fetch("project_id").to_i
     id = volunteer.fetch("id").to_i
     Volunteer.new({:name => name, :project_id => project_id, :id => id})
   end
 
   def self.find_by_project(proj_id)
     volunteers = []
-    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE projects_id = #{proj_id};")
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{proj_id};")
     returned_volunteers.each() do |volunteer|
       name = volunteer.fetch("name")
       id = volunteer.fetch("id").to_i
